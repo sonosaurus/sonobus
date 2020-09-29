81092d6cd6d9698c09fdbad09bbf79c70c5ae73d
Jesse Chappell <jesse@sonosaurus.com>
2020-09-29 02:57:21 -0400

    added JuceLibraryCode to repo, so Projucer does not have to be used by folks

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,171 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   bar_click_wav;
+    const int            bar_click_wavSize = 2846;
+
+    extern const char*   beat_click_wav;
+    const int            beat_click_wavSize = 2708;
+
+    extern const char*   copy_icon_svg;
+    const int            copy_icon_svgSize = 2218;
+
+    extern const char*   DejaVuSans_ttf;
+    const int            DejaVuSans_ttfSize = 757076;
+
+    extern const char*   dice_icon_128_png;
+    const int            dice_icon_128_pngSize = 3793;
+
+    extern const char*   dots_svg;
+    const int            dots_svgSize = 3598;
+
+    extern const char*   dots_icon_png;
+    const int            dots_icon_pngSize = 362;
+
+    extern const char*   folder_icon_svg;
+    const int            folder_icon_svgSize = 4871;
+
+    extern const char*   hearothers_svg;
+    const int            hearothers_svgSize = 9200;
+
+    extern const char*   incoming_allowed_svg;
+    const int            incoming_allowed_svgSize = 9780;
+
+    extern const char*   incoming_allowed_active_svg;
+    const int            incoming_allowed_active_svgSize = 9888;
+
+    extern const char*   incoming_disallowed_svg;
+    const int            incoming_disallowed_svgSize = 9407;
+
+    extern const char*   lgc_bar_wav;
+    const int            lgc_bar_wavSize = 806;
+
+    extern const char*   link_svg;
+    const int            link_svgSize = 3128;
+
+    extern const char*   list_icon1_png;
+    const int            list_icon1_pngSize = 3637;
+
+    extern const char*   loop_icon_svg;
+    const int            loop_icon_svgSize = 4901;
+
+    extern const char*   met_svg;
+    const int            met_svgSize = 3784;
+
+    extern const char*   mic_svg;
+    const int            mic_svgSize = 4186;
+
+    extern const char*   mic_disabled_svg;
+    const int            mic_disabled_svgSize = 4490;
+
+    extern const char*   mic_pointing_svg;
+    const int            mic_pointing_svgSize = 13723;
+
+    extern const char*   move_updown_svg;
+    const int            move_updown_svgSize = 4337;
+
+    extern const char*   muteothers_svg;
+    const int            muteothers_svgSize = 8680;
+
+    extern const char*   outgoing_allowed_svg;
+    const int            outgoing_allowed_svgSize = 10153;
+
+    extern const char*   outgoing_allowed_active_svg;
+    const int            outgoing_allowed_active_svgSize = 4065;
+
+    extern const char*   outgoing_disallowed_svg;
+    const int            outgoing_disallowed_svgSize = 9997;
+
+    extern const char*   paste_icon_svg;
+    const int            paste_icon_svgSize = 2211;
+
+    extern const char*   pause_icon_svg;
+    const int            pause_icon_svgSize = 4016;
+
+    extern const char*   people_png;
+    const int            people_pngSize = 1757;
+
+    extern const char*   people_svg;
+    const int            people_svgSize = 5498;
+
+    extern const char*   person_png;
+    const int            person_pngSize = 965;
+
+    extern const char*   person_svg;
+    const int            person_svgSize = 3615;
+
+    extern const char*   play_icon_svg;
+    const int            play_icon_svgSize = 3768;
+
+    extern const char*   power_svg;
+    const int            power_svgSize = 3120;
+
+    extern const char*   power_sel_svg;
+    const int            power_sel_svgSize = 3133;
+
+    extern const char*   record_svg;
+    const int            record_svgSize = 3817;
+
+    extern const char*   record_active_svg;
+    const int            record_active_svgSize = 3805;
+
+    extern const char*   record_active_alt_svg;
+    const int            record_active_alt_svgSize = 3829;
+
+    extern const char*   send_group_svg;
+    const int            send_group_svgSize = 6905;
+
+    extern const char*   send_group_small_svg;
+    const int            send_group_small_svgSize = 6910;
+
+    extern const char*   settings_icon_svg;
+    const int            settings_icon_svgSize = 4898;
+
+    extern const char*   skipback_icon_svg;
+    const int            skipback_icon_svgSize = 2211;
+
+    extern const char*   sonobus_logo_96_png;
+    const int            sonobus_logo_96_pngSize = 5099;
+
+    extern const char*   sonobus_title_small_png;
+    const int            sonobus_title_small_pngSize = 16322;
+
+    extern const char*   speaker_svg;
+    const int            speaker_svgSize = 3742;
+
+    extern const char*   speaker_disabled_svg;
+    const int            speaker_disabled_svgSize = 3851;
+
+    extern const char*   urei_main_wav;
+    const int            urei_main_wavSize = 632;
+
+    extern const char*   wordmaker_g;
+    const int            wordmaker_gSize = 125047;
+
+    extern const char*   x_icon_svg;
+    const int            x_icon_svgSize = 4297;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 48;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
