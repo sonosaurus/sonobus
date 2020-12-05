faf7c9082169df10a204435784e82c018b2031f7
Zipdox <zipdox@protonmail.com>
2020-12-05 16:17:24 +0100

    Linux install/uninstall script.

---
+++ Builds/LinuxMakefile/install.sh
@@ -0,0 +1,4 @@
+cp build/SonoBus /usr/local/bin/SonoBus
+cp sonobus.desktop /usr/local/share/applications/sonobus.desktop
+chmod +x /usr/local/share/applications/sonobus.desktop
+cp ../../images/sonobus_logo@2x.png /usr/share/pixmaps/sonobus.png
