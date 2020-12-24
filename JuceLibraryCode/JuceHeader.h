6dcdd0391a9680c5f89c16520ee559d722d16502
Jesse Chappell <jesse@sonosaurus.com>
2020-12-24 00:28:36 -0500

    update version to 1.2.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SonoBus";
     const char* const  companyName    = "Sonosaurus";
-    const char* const  versionString  = "1.2.1";
-    const int          versionNumber  = 0x10201;
+    const char* const  versionString  = "1.2.2";
+    const int          versionNumber  = 0x10202;
 }
 #endif
