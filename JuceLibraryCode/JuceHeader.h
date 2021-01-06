f8e754de034dab718b6e7bc71f2ec76d18a46a20
Jesse Chappell <jesse@sonosaurus.com>
2021-01-06 14:08:10 -0500

    update version to 1.3.2

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SonoBus";
     const char* const  companyName    = "Sonosaurus";
-    const char* const  versionString  = "1.3.1";
-    const int          versionNumber  = 0x10301;
+    const char* const  versionString  = "1.3.2";
+    const int          versionNumber  = 0x10302;
 }
 #endif
--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                1.3.1
+ #define JucePlugin_Version                1.3.2
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x10301
+ #define JucePlugin_VersionCode            0x10302
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "1.3.1"
+ #define JucePlugin_VersionString          "1.3.2"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
