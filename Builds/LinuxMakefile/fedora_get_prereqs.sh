87f976ccaf348f8b0b8fef66873b924cfd2ddacc
Billy Rooke <billy@fedora-ssd.home>
2021-02-02 22:10:07 +0000

    Add Fedora prerequisite script

---
+++ Builds/LinuxMakefile/fedora_get_prereqs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Install prerequisite packages for SonoBus build on Fedora
+
+GITREPO="https://github.com/essej/sonobus.git"
+
+PREREQS="libopusenc \
+	libopusenc-devel \
+	jack-audio-connection-kit \
+	jack-audio-connection-kit-devel \
+	alsa-lib-devel \
+	libX11-devel \
+	libXext-devel \
+	libXinerama-devel \
+	libXrandr-devel \
+	libXcursor-devel \
+	mesa-libGL-devel \
+	freetype-devel \
+	libcurl-devel
+        "
+
+echo ""
+echo "Installing prerequisites - " $(date)
+echo ""
+
+sudo dnf update
+sudo dnf -y groupinstall "Development Tools" "Development Libraries"
+sudo dnf -y install git $PREREQS 
+
+
+
