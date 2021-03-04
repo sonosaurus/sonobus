68daa503254cea4ffa4d91d82511e73d3b977df3
Jesse Chappell <jesse@sonosaurus.com>
2021-03-04 01:39:53 -0500

    update version to 1.3.7

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                1.3.6
+ #define JucePlugin_Version                1.3.7
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x10306
+ #define JucePlugin_VersionCode            0x10307
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "1.3.6"
+ #define JucePlugin_VersionString          "1.3.7"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
