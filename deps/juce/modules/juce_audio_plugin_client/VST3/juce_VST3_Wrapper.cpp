aefb385571d76de49b2a2371889bf676205b1dcf
Jesse Chappell <jesse@sonosaurus.com>
2021-03-06 12:51:04 -0500

    juce: apply fix for resize recursion when using ardour on linux, hack for now

--- deps/juce/modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
+++ deps/juce/modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp
@@ -1306,6 +1306,8 @@ private:
                         cubase10Workaround->triggerAsyncUpdate();
                     }
                     else
+                   #elif JUCE_LINUX
+                    if (!getHostType().isArdour())
                    #endif
                     {
                         if (auto* peer = component->getPeer())
