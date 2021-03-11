915b7b00806ddf8efebafc432531e0ff7b9520fa
Jesse Chappell <jesse@sonosaurus.com>
2021-03-11 14:26:59 -0500

    updated linux scripts for new all lowercase filenames

--- Builds/LinuxMakefile/install.sh
+++ Builds/LinuxMakefile/install.sh
@@ -9,11 +9,15 @@ fi
 echo "Installing SonoBus to ${PREFIX} ... (specify destination as command line argument if you want it elsewhere)"
 
 mkdir -p ${PREFIX}/bin
-if ! cp build/SonoBus  ${PREFIX}/bin/SonoBus ; then
+
+if ! cp build/sonobus  ${PREFIX}/bin/sonobus ; then
   echo
   echo "Looks like you need to run this as 'sudo $0'"
   exit 2
 fi
+
+# remove old name
+rm -rf ${PREFIX}/bin/SonoBus
 
 mkdir -p ${PREFIX}/share/applications
 cp  sonobus.desktop ${PREFIX}/share/applications/sonobus.desktop
@@ -22,9 +26,12 @@ chmod +x ${PREFIX}/share/applications/sonobus.desktop
 mkdir -p ${PREFIX}/share/pixmaps
 cp  ../../images/sonobus_logo@2x.png ${PREFIX}/share/pixmaps/sonobus.png
 
-if [ -d build/SonoBus.vst3 ] ; then
+# remove old name
+rm -rf ${PREFIX}/lib/vst3/SonoBus.vst3 
+
+if [ -d build/sonobus.vst3 ] ; then
   mkdir -p ${PREFIX}/lib/vst3
-  cp -a build/SonoBus.vst3 ${PREFIX}/lib/vst3/
+  cp -a build/sonobus.vst3 ${PREFIX}/lib/vst3/
 
   echo "SonoBus VST3 plugin installed"
 fi
