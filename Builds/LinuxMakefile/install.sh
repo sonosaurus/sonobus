fdd8b36eeba32024dee433d7c8b3165200d025dc
Zipdox <zipdox@protonmail.com>
2021-03-25 18:41:10 +0100

    Unified prereq script and added deb build script

--- Builds/LinuxMakefile/install.sh
+++ Builds/LinuxMakefile/install.sh
@@ -9,29 +9,22 @@ fi
 echo "Installing SonoBus to ${PREFIX} ... (specify destination as command line argument if you want it elsewhere)"
 
 mkdir -p ${PREFIX}/bin
-
-if ! cp build/sonobus  ${PREFIX}/bin/sonobus ; then
+if ! cp build/SonoBus  ${PREFIX}/bin/SonoBus ; then
   echo
   echo "Looks like you need to run this as 'sudo $0'"
   exit 2
 fi
 
-# remove old name
-rm -rf ${PREFIX}/bin/SonoBus
-
 mkdir -p ${PREFIX}/share/applications
-cp  sonobus.desktop ${PREFIX}/share/applications/sonobus.desktop
+cp sonobus.desktop ${PREFIX}/share/applications/sonobus.desktop
 chmod +x ${PREFIX}/share/applications/sonobus.desktop
 
 mkdir -p ${PREFIX}/share/pixmaps
-cp  ../../images/sonobus_logo@2x.png ${PREFIX}/share/pixmaps/sonobus.png
+cp ../../images/sonobus_logo@2x.png ${PREFIX}/share/pixmaps/sonobus.png
 
-# remove old name
-rm -rf ${PREFIX}/lib/vst3/SonoBus.vst3 
-
-if [ -d build/sonobus.vst3 ] ; then
+if [ -d build/SonoBus.vst3 ] ; then
   mkdir -p ${PREFIX}/lib/vst3
-  cp -a build/sonobus.vst3 ${PREFIX}/lib/vst3/
+  cp -a build/SonoBus.vst3 ${PREFIX}/lib/vst3/
 
   echo "SonoBus VST3 plugin installed"
 fi
