f3b62f0a9b958c82ce76f6fbf6b161288c2f184c
Jesse Chappell <jesse@sonosaurus.com>
2021-04-05 16:52:27 -0400

    reworked linux install and uninstall to work with cmake build

---
+++ linuxinstall.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PREFIX=/usr/local
+
+if [ -n "$1" ] ; then
+  PREFIX="$1"
+fi
+
+echo "Installing SonoBus to ${PREFIX} ... (specify destination as command line argument if you want it elsewhere)"
+
+BUILDDIR=build/SonoBus_artefacts/Release
+INSTBUILDDIR=build/SonoBusInst_artefacts/Release
+
+mkdir -p ${PREFIX}/bin
+if ! cp ${BUILDDIR}/Standalone/sonobus  ${PREFIX}/bin/sonobus ; then
+  echo
+  echo "Looks like you need to run this as 'sudo $0'"
+  exit 2
+fi
+
+mkdir -p ${PREFIX}/share/applications
+cp scripts/sonobus.desktop ${PREFIX}/share/applications/sonobus.desktop
+chmod +x ${PREFIX}/share/applications/sonobus.desktop
+
+mkdir -p ${PREFIX}/share/pixmaps
+cp images/sonobus_logo@2x.png ${PREFIX}/share/pixmaps/sonobus.png
+
+if [ -d ${BUILDDIR}/VST3/SonoBus.vst3 ] ; then
+  mkdir -p ${PREFIX}/lib/vst3
+  cp -a ${BUILDDIR}/VST3/SonoBus.vst3 ${PREFIX}/lib/vst3/
+
+  echo "SonoBus VST3 plugin installed"
+fi
+
+if [ -d ${INSTBUILDDIR}/VST3/SonoBusInstrument.vst3 ] ; then
+  mkdir -p ${PREFIX}/lib/vst3
+  cp -a ${INSTBUILDDIR}/VST3/SonoBusInstrument.vst3 ${PREFIX}/lib/vst3/
+
+  echo "SonoBus VST3i plugin installed"
+fi
+
+echo "SonoBus application installed"
+
---
+++ linuxuninstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PREFIX=/usr/local
+
+if [ -n "$1" ] ; then
+  PREFIX="$1"
+fi
+
+echo "Un-Installing SonoBus from ${PREFIX} ... (specify destination as command line argument if you have it elsewhere)"
+
+# remove old binary name
+if [ -f ${PREFIX}/bin/SonoBus ] ; then
+  if ! rm -f ${PREFIX}/bin/SonoBus ; then
+    echo
+    echo "Looks like you need to run this with 'sudo $0'"
+    exit 2
+  fi
+fi
+
+if [ -f ${PREFIX}/bin/sonobus ] ; then
+  if ! rm -f ${PREFIX}/bin/sonobus ; then
+    echo
+    echo "Looks like you need to run this with 'sudo $0'"
+    exit 2
+  fi
+fi
+
+rm -f ${PREFIX}/share/applications/sonobus.desktop
+rm -f ${PREFIX}/pixmaps/sonobus.png
+
+rm -rf ${PREFIX}/lib/vst3/SonoBus.vst3
+rm -rf ${PREFIX}/lib/vst3/SonoBusInstrument.vst3
+# remove old VST name
+rm -rf ${PREFIX}/lib/vst3/sonobus.vst3
+
+echo "SonoBus uninstalled"
