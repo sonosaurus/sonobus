953bde5f1ca1c995bd1a3b0b124b6f936cec170d
Jesse Chappell <jesse@sonosaurus.com>
2021-04-05 20:29:29 -0400

    moved mobile build locations

--- mobile/JuceLibraryCode/JuceHeader.h
+++ mobile/JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SonoBus";
     const char* const  companyName    = "Sonosaurus";
-    const char* const  versionString  = "1.4.3";
-    const int          versionNumber  = 0x10403;
+    const char* const  versionString  = "1.4.4";
+    const int          versionNumber  = 0x10404;
 }
 #endif
