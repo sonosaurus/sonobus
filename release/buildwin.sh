6e87f2b1b3e990ce05b6ff9378da9af8bc4abe5f
Jesse Chappell <jesse@sonosaurus.com>
2021-04-06 00:28:53 -0400

    removed old buildwin.sh stuff

--- release/buildwin.sh
+++ release/buildwin.sh
@@ -2,40 +2,12 @@
 
 cd ..
 
-./setupcmakewin.sh
+rm -rf build build32
 
-cmake --build build --target clean
-cmake --build build32 --target clean
+./setupcmakewin.sh
 
 ./buildcmake.sh
 
-
-exit
-
-cd ../Builds/VisualStudio2017
-
-
-#ARGS="-v:m  -m  -noWarn:C4244,C4267,C4996,C4305,C4189,C4100,C4127,C4458"
-ARGS="-m  /clp:ErrorsOnly  -v:m"
-
-# build everything in Release mode
-echo "Building 64 bit"
-
-MSBuild.exe SonoBus.sln ${ARGS} -t:Rebuild /p:Configuration=Release /p:Platform=x64
-
-# build 32 bit
-echo "Building 32 bit"
-
-#MSBuild.exe SonoBus.sln ${ARGS} -t:Rebuild /p:Configuration=Release32 /p:Platform=Win32
-MSBuild.exe SonoBus_SharedCode.vcxproj ${ARGS}  -t:Rebuild /p:Configuration=Release32 /p:Platform=Win32
-MSBuild.exe SonoBus_StandalonePlugin.vcxproj ${ARGS} -t:Rebuild /p:Configuration=Release32 /p:Platform=Win32
-MSBuild.exe SonoBus_VST.vcxproj ${ARGS} -t:Rebuild /p:Configuration=Release32 /p:Platform=Win32
-MSBuild.exe SonoBus_VST3.vcxproj ${ARGS} -t:Rebuild /p:Configuration=Release32 /p:Platform=Win32
-
-
-# build VST2 with ReleaseVST2
-#MSBuild.exe SonoBus_SharedCode.vcxproj ${ARGS}  -t:Rebuild /p:Configuration=ReleaseVST2 /p:Platform=x64
-#MSBuild.exe SonoBus_VST.vcxproj ${ARGS} -t:Rebuild /p:Configuration=ReleaseVST2 /p:Platform=x64
 
 
 
