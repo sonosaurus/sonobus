d1a7b386f8c4775ddf7ab771a30f53da20ef1fb6
Jesse Chappell <jesse@sonosaurus.com>
2021-04-15 00:18:53 -0400

    update version to 1.4.5 (68)

--- mobile/JuceLibraryCode/JuceHeader.h
+++ mobile/JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SonoBus";
     const char* const  companyName    = "Sonosaurus";
-    const char* const  versionString  = "1.4.4";
-    const int          versionNumber  = 0x10404;
+    const char* const  versionString  = "1.4.5";
+    const int          versionNumber  = 0x10405;
 }
 #endif
