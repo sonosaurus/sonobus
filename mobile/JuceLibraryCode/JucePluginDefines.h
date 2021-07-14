b2d7fbf02a20a20041270fed13c836aa6b57c415
Jesse Chappell <jesse@sonosaurus.com>
2021-07-14 12:33:01 -0400

    updated mobile version

--- mobile/JuceLibraryCode/JucePluginDefines.h
+++ mobile/JuceLibraryCode/JucePluginDefines.h
@@ -74,13 +74,13 @@
  #define JucePlugin_EditorRequiresKeyboardFocus  0
 #endif
 #ifndef  JucePlugin_Version
- #define JucePlugin_Version                1.4.5
+ #define JucePlugin_Version                1.4.6
 #endif
 #ifndef  JucePlugin_VersionCode
- #define JucePlugin_VersionCode            0x10405
+ #define JucePlugin_VersionCode            0x10406
 #endif
 #ifndef  JucePlugin_VersionString
- #define JucePlugin_VersionString          "1.4.5"
+ #define JucePlugin_VersionString          "1.4.6"
 #endif
 #ifndef  JucePlugin_VSTUniqueID
  #define JucePlugin_VSTUniqueID            JucePlugin_PluginCode
