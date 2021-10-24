9c1529955ac35a77c2af0c04ec1a79ac3fec1215
Hannah Schellekens <github@hannahschellekens.nl>
2021-10-24 13:11:30 +0200

    Added title panel to soundboard view and refactored SoundboardView.

--- Source/SoundboardView.cpp
+++ Source/SoundboardView.cpp
@@ -9,16 +9,55 @@ SoundboardView::SoundboardView()
 {
     setOpaque(true);
 
-    mTitleLabel = std::make_unique<Label>("title", TRANS("Soundboard"));
-    mTitleLabel->setJustificationType(Justification::centred);
-    mTitleLabel->setFont(Font(18, Font::bold));
-    mTitleLabel->setColour(Label::textColourId, Colour(0xeeffffff));
+    createSoundboardTitle();
+    createBasePanels();
+}
 
-    addAndMakeVisible(mTitleLabel.get());
+void SoundboardView::createBasePanels()
+{
+    soundboardContainerBox.items.clear();
+    soundboardContainerBox.flexDirection = FlexBox::Direction::column;
+    soundboardContainerBox.items.add(FlexItem(TITLE_LABEL_WIDTH, TITLE_HEIGHT, titleBox).withMargin(0).withFlex(0));
+    soundboardContainerBox.items.add(FlexItem(ELEMENT_MARGIN, ELEMENT_MARGIN).withMargin(0));
 
     mainBox.items.clear();
     mainBox.flexDirection = FlexBox::Direction::row;
-    mainBox.items.add(FlexItem(90, 4, *mTitleLabel).withMargin(0).withFlex(1));
+    mainBox.items.add(FlexItem(TITLE_LABEL_WIDTH, ELEMENT_MARGIN, soundboardContainerBox).withMargin(0).withFlex(1));
+}
+
+void SoundboardView::createSoundboardTitle()
+{
+    createSoundboardTitleLabel();
+    createSoundboardTitleCloseButton();
+
+    titleBox.items.clear();
+    titleBox.flexDirection = FlexBox::Direction::row;
+    titleBox.items.add(FlexItem(ELEMENT_MARGIN, ELEMENT_MARGIN).withMargin(0).withFlex(0));
+    titleBox.items.add(FlexItem(MENU_BUTTON_WIDTH, ELEMENT_MARGIN, *mCloseButton).withMargin(0).withFlex(0));
+    titleBox.items.add(FlexItem(TITLE_LABEL_WIDTH, ELEMENT_MARGIN, *mTitleLabel).withMargin(0).withFlex(1));
+    titleBox.items.add(FlexItem(ELEMENT_MARGIN, ELEMENT_MARGIN).withMargin(0).withFlex(0));
+}
+
+void SoundboardView::createSoundboardTitleLabel()
+{
+    mTitleLabel = std::make_unique<Label>("title", TRANS("Soundboard"));
+    mTitleLabel->setJustificationType(Justification::centred);
+    mTitleLabel->setFont(Font(TITLE_FONT_HEIGHT, Font::bold));
+    mTitleLabel->setColour(Label::textColourId, Colour(0xeeffffff));
+    addAndMakeVisible(mTitleLabel.get());
+}
+
+void SoundboardView::createSoundboardTitleCloseButton()
+{
+    mCloseButton = std::make_unique<SonoDrawableButton>("x", DrawableButton::ButtonStyle::ImageFitted);
+    std::unique_ptr<Drawable> imageCross(Drawable::createFromImageData(BinaryData::x_icon_svg, BinaryData::x_icon_svgSize));
+    mCloseButton->setImages(imageCross.get());
+    mCloseButton->setTitle(TRANS("Close Soundboard"));
+    mCloseButton->setColour(DrawableButton::backgroundColourId, Colours::transparentBlack);
+    mCloseButton->onClick = [this]() {
+        setVisible(false);
+    };
+    addAndMakeVisible(mCloseButton.get());
 }
 
 void SoundboardView::paint(Graphics& g)
--- Source/SoundboardView.h
+++ Source/SoundboardView.h
@@ -7,7 +7,13 @@
 #include <JuceHeader.h>
 
 #include "SonobusPluginProcessor.h"
+#include "SonoDrawableButton.h"
 
+/**
+ * User Interface element for the Soundboard.
+ *
+ * @author Hannah Schellekens, Sten Wessel
+ */
 class SoundboardView : public Component
 {
 public:
@@ -17,10 +23,56 @@ public:
     void resized() override;
 
 private:
+    constexpr static const float MENU_BUTTON_WIDTH = 36;
+    constexpr static const float TITLE_LABEL_WIDTH = 90;
+    constexpr static const float TITLE_HEIGHT = 32;
+    constexpr static const float TITLE_FONT_HEIGHT = 18;
+    constexpr static const float ELEMENT_MARGIN = 4;
+
+    /**
+     * The outer soundboard panel box.
+     */
     FlexBox mainBox;
 
+    /**
+     * Wrapper for soundboard UI contents.
+     */
+    FlexBox soundboardContainerBox;
+
+    /**
+     * Title bar of the soundboard panel.
+     */
+    FlexBox titleBox;
+
+    /**
+     * Label showing the panel title.
+     */
     std::unique_ptr<Label> mTitleLabel;
 
+    /**
+     * Button that closes the soundboard panel.
+     */
+    std::unique_ptr<SonoDrawableButton> mCloseButton;
+
+    /**
+     * Creates the outer UI panels.
+     */
+    void createBasePanels();
+
+    /**
+     * Creates the title UI elements of the soundboard panel.
+     */
+    void createSoundboardTitle();
+
+    /**
+     * Creates the title label.
+     */
+    void createSoundboardTitleLabel();
+
+    /**
+     * Creates the close button of the title panel.
+     */
+    void createSoundboardTitleCloseButton();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SoundboardView)
 };
