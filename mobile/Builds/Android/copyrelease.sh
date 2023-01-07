f3a8837fc01dc8ad914c3b4e6bb08e1e08febcbc
essej <jesse@essej.net>
2023-01-07 00:40:07 -0500

    last version updates

--- mobile/Builds/Android/copyrelease.sh
+++ mobile/Builds/Android/copyrelease.sh
@@ -10,3 +10,11 @@ VERSION=$1
 FNAME=sonobus-$VERSION-android.apk
 
 cp -v app/build/outputs/apk/release_/release/app-release_-release.apk ../../../release/android/$FNAME
+
+
+# now make a zip of the symbols
+DFNAME=debug_${FNAME/apk/zip}
+rm -f ../../../release/android/${DFNAME}
+
+cd app/build/intermediates/cmake/release_Release/obj
+zip -r ../../../../../../../../../release/android/${DFNAME} */*.so
