5801be25e0301c93520e8c09da26cb8f0d0d6106
essej <jesse@essej.net>
2023-02-25 22:35:38 -0500

    update mobile version numbers

--- mobile/JuceLibraryCode/JuceHeader.h
+++ mobile/JuceLibraryCode/JuceHeader.h
@@ -53,7 +53,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "SonoBus";
     const char* const  companyName    = "Sonosaurus";
-    const char* const  versionString  = "1.6.1";
-    const int          versionNumber  = 0x10601;
+    const char* const  versionString  = "1.6.2";
+    const int          versionNumber  = 0x10602;
 }
 #endif
