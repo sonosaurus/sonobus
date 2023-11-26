cec68f43b0f0310bb9768f7a227bb36f68b60f7a
essej <jesse@essej.net>
2023-11-26 10:22:33 -0500

    git subrepo pull (merge) deps/aoo
    subrepo:
      subdir:   "deps/aoo"
      merged:   "e7d009f732"
    upstream:
      origin:   "https://github.com/essej/aoo.git"
      branch:   "sono"
      commit:   "de76f3f78e"
    git-subrepo:
      version:  "0.4.5"
      origin:   "https://github.com/ingydotnet/git-subrepo.git"
      commit:   "dbb99be"

--- deps/aoo/lib/src/server.cpp
+++ deps/aoo/lib/src/server.cpp
@@ -908,6 +908,46 @@ client_endpoint::client_endpoint(server &s, int sock, const ip_address &addr)
         // ignore
     }
 
+    // keepalive and user timeout
+    int keepidle = 10;
+    int keepinterval = 10;
+    int keepcnt = 2;
+
+    val = 1;
+    if (setsockopt (socket, SOL_SOCKET, SO_KEEPALIVE, (char*) &val, sizeof (val)) < 0){
+        LOG_WARNING("client_endpoint: couldn't set SO_KEEPALIVE");
+        // ignore
+    }
+#ifdef TCP_KEEPIDLE
+    if (setsockopt (socket, IPPROTO_TCP, TCP_KEEPIDLE, (char*) &keepidle, sizeof (keepidle)) < 0){
+        LOG_WARNING("client_endpoint: couldn't set SO_KEEPIDLE");
+        // ignore
+    }
+#elif defined(TCP_KEEPALIVE)
+    if (setsockopt (socket, IPPROTO_TCP, TCP_KEEPALIVE, (char*) &keepidle, sizeof (keepidle)) < 0){
+        LOG_WARNING("client_endpoint: couldn't set SO_KEEPALIVE");
+        // ignore
+    }
+#endif
+
+    if (setsockopt (socket, IPPROTO_TCP, TCP_KEEPINTVL, (char*) &keepinterval, sizeof (keepinterval)) < 0){
+        LOG_WARNING("client_endpoint: couldn't set SO_KEEPINTVL");
+        // ignore
+    }
+    if (setsockopt (socket, IPPROTO_TCP, TCP_KEEPCNT, (char*) &keepcnt, sizeof (keepcnt)) < 0){
+        LOG_WARNING("client_endpoint: couldn't set SO_KEEPCNT");
+        // ignore
+    }
+
+#ifdef TCP_USER_TIMEOUT
+    int utimeout = (keepidle + keepinterval * keepcnt - 1) * 1000;
+    // attempt to set TCP_USER_TIMEOUT option
+    if (setsockopt (socket, IPPROTO_TCP, TCP_USER_TIMEOUT, (char*) &utimeout, sizeof (utimeout)) < 0){
+        LOG_WARNING("client_endpoint: couldn't set TCP_USER_TIMEOUT");
+        // ignore
+    }
+#endif
+
     sendbuffer_.setup(65536);
     recvbuffer_.setup(65536);
 
